fa8db0de481657c0bc2c71ec07dfa671b5beec12
Bryant <aragon1192@gmail.com>
2026-08-08 21:00:11 -0400

    added manual hexadecimal conversion

--- task.py
+++ task.py
@@ -7,3 +7,14 @@ def conv_endian(num, endian='big'):
     if num < 0:
         is_negative = True
         num = -num
+
+    hex_digits = "0123456789ABCDEF"
+    hex_string = ""
+
+    if num == 0:
+        hex_string = "00"
+    else:
+        while num > 0:
+            remainder = num % 16
+            hex_string = hex_digits[remainder] + hex_string
+            num = num // 16
